2aa468d21bd60945b7f3e5f5bc878bd3a46fb44e
maukoshiki <maukoshiki324@gmail.com>
2025-10-17 21:32:58 +0900

    エラーハンドリング

--- shared/lib/API/client.py
+++ shared/lib/API/client.py
@@ -1,7 +1,11 @@
+from shared.lib.basicError.src.main import BasicError
+from shared.lib.basicError.src.main import errorWrapper
 import requests
 
 # TODO: エラーハンドリング
 class Client:
+
+    @errorWrapper("InvalidFormat")
     def __init__(self, key: str = None):
         self.session = requests.Session()
         self.session.headers.update({
@@ -12,26 +16,31 @@ class Client:
             self.session.headers.update({
                 "Authorization": f"Bearer {key}"
             })
-
+            
+    @errorWrapper("NetworkConnectionFailed")
     def get(self, url:str, params:dict = None) -> dict:
         response = self.session.get(url, params=params)
 
         return self._handle_response(response)
     
+    @errorWrapper("NetworkConnectionFailed")
     def post(self, url: str, data: dict = None) -> dict:
         response = self.session.post(url, json=data)
 
         return self._handle_response(response)
-
+    
+    @errorWrapper("NetworkConnectionFailed")
     def put(self, url:str, data:dict = None) -> dict:
         response = self.session.put(url, json=data)
 
         return self._handle_response(response)
     
+    @errorWrapper("NetworkConnectionFailed")
     def delete(self, url: str, data: dict = None) -> dict:
         response = self.session.delete(url, json=data)
 
         return self._handle_response(response)
+    
     
     def _handle_response(self, response: requests.Response) -> dict:
         try:
