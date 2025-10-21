13cce78f3084bf6210793695f8c1e9e5942201f5
towa0829 <towa0000829@icloud.com>
2025-10-21 23:09:56 +0900

    feat: add mentor API routing definitions

--- services/mentor_service/app/route/api/v1.py
+++ services/mentor_service/app/route/api/v1.py
@@ -1,4 +1,22 @@
 from fastapi import APIRouter
+from schemas import (
+    NewMentorRequest, NewMentorResponse,MentorResponse,
+    UpdateMentorRequest, NewMentorGroupRequest,NewMentorGroupResponse,
+    MentorGroupResponse,UpdataMentorGroupRequest, MentorToGroupRequest,
+    MentorToGroupResponse
+)
 
-# /api/v1/job
-router = APIRouter()
+# /api/v1/mentor
+
+router = APIRouter(prefix="/v1", tags=["mentor"])
+
+# =============
+# Mentor Admin
+# =============
+
+@router.post("/admin/new",response_model=NewMentorResponse)
+async def create_mentor(request: NewMentorRequest):
+    """新しいMentorアカウントを作成する"""
+    pass
+
+
