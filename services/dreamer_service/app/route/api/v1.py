193f1cb0257d1f2c03aae928576e6595b19976a4
towa0829 <towa0000829@icloud.com>
2025-10-29 11:14:50 +0900

    define mentor's DbOperation. modify typo. make chief_mentor and access_group nullable.

--- services/dreamer_service/app/route/api/v1.py
+++ services/dreamer_service/app/route/api/v1.py
@@ -21,11 +21,8 @@ router = APIRouter()
 @router.post("/admin/new", response_model=NewDreamerResponse)
 async def create_dreamer(request: NewDreamerRequest):
     """新しいアカウントの作成"""
-
     _, result, error = dreamer_crud.create(DreamerTableSchema(**request.model_dump(), login_id = random_string()))
-
     print(error, flush=True)
-
     return NewDreamerResponse.model_validate(result)
 
 @router.get("/admin/{dreamer_id}", response_model=DreamerResponse)
@@ -36,9 +33,7 @@ async def get_dreamer(dreamer_id: str):
             ["dreamer_id", "==", dreamer_id]
         ] 
     )
-
     print(error, flush=True)
-
     return DreamerResponse.model_validate(result[0])
 
 @router.put("/admin/{dreamer_id}", response_model=DreamerResponse)
@@ -50,9 +45,7 @@ async def update_dreamer(dreamer_id: str, request: UpdateDreamerRequest):
         ], 
     request.model_dump(exclude_unset=True)
     )
-
     print(error, flush=True)
-
     return DreamerResponse.model_validate(result[0])
 
 @router.delete("/admin/{dreamer_id}", response_model=DreamerResponse)
@@ -63,9 +56,7 @@ async def delete_dreamer(dreamer_id: str):
             ["dreamer_id", "==", dreamer_id]
         ]
     )
-
     print(error, flush=True)
-
     return DreamerResponse.model_validate(result[0])
 
 # ================
@@ -79,7 +70,6 @@ async def create_group(request: NewDreamerGroupRequest):
     group_instance = DreamerGroupTableSchema(**group_data)
     _, group_result, error = dreamer_group_crud.create(group_instance)
     print(error, flush=True)
-
     group_id = group_result.group_id
 
     # メンバー登録
@@ -105,20 +95,16 @@ async def get_group(group_id: str):
             ["group_id","==",group_id]
         ]
     )
-
     print(error, flush=True)
 
     group=group_result[0]
-
     _, members, error=dreamer_group_members_crud.read(
         [
             ["group_id", "==", group_id]
         ]
     )
     dreamers=[DreamerInGroup(name=member.name, dreamer_id=member.dreamer_id)for member in members]
-
     print(error, flush=True)
-
     return DreamerGroupResponse(
         name=group.name,
         description=group.description,
@@ -134,9 +120,7 @@ async def update_group(group_id: str, request: UpdateDreamerGroupRequest):
         ],
         request.model_dump(exclude_unset=True)
     )
-
     print(error, flush=True)
-
     return DreamerGroupResponse.model_validate(result[0])
 
 @router.delete("/groups/{group_id}", response_model=DreamerGroupResponse)
@@ -147,9 +131,7 @@ async def delete_group(group_id: str):
             ["group_id", "==", group_id]
         ]
     )
-    print("group_id:", group_id)
     print(error, flush=True)
-
     return DreamerGroupResponse.model_validate(result[0])
 
 #================
@@ -160,6 +142,7 @@ async def delete_group(group_id: str):
 async def add_dreamer_to_group(group_id: str, request: DreamerToGroupRequest):
     """グループにdreamerを追加"""
     added_dreamers = []
+
     for dreamer_id in request.dreamers:
         member_instance = DreamerGroupMembersTableSchema(
             group_id=group_id,
@@ -188,5 +171,4 @@ async def remove_dreamer_from_group(group_id: str, request: DreamerToGroupReques
         print(error, flush=True)
         if result:
             deleted_ids.append(dreamer_id)
-
     return DreamerToGroupResponse.model_validate({"dreamers": deleted_ids})
--- services/mentor_service/app/route/api/v1.py
+++ services/mentor_service/app/route/api/v1.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter
+
 from crud import mentors_crud, mentor_groups_crud, mentor_group_members_crud
 from schemas import (
-    NewMentorRequest, NewMentorResponse,MentorResponse,
-    UpdateMentorRequest, NewMentorGroupRequest,NewMentorGroupResponse,
-    MentorGroupResponse,UpdateMentorGroupRequest, MentorToGroupRequest,
-    MentorToGroupResponse
+    NewMentorRequest, NewMentorResponse, UpdateMentorRequest, MentorResponse,
+    NewMentorGroupRequest, NewMentorGroupResponse, MentorGroupResponse,
+    UpdateMentorGroupRequest, MentorToGroupRequest, MentorToGroupResponse, 
+    MentorInGroup,MentorRoleInfo
 )
+from models.MentorsTable import MentorTableSchema
+from models.MentorGroupsTable import MentorGroupTableSchema
+from shared.utils.security import random_string
 
 # /api/v1/mentor
-
 router = APIRouter()
 
 # ====================
@@ -18,40 +21,48 @@ router = APIRouter()
 @router.post("/admin/new", response_model=NewMentorResponse)
 async def create_mentor(request: NewMentorRequest):
     """新しいMentorアカウントを作成"""
-    _, result, _=mentors_crud.create(request)
-    return result
+    _, result, error = mentors_crud.create(
+        MentorTableSchema(**request.model_dump(), login_id=random_string())
+    )
+    print(error, flush=True)
+    return NewMentorResponse.model_validate(result)
+
 
 @router.get("/admin/{mentor_id}", response_model=MentorResponse)
 async def get_mentor(mentor_id: str):
     """Mentor情報を取得"""
-    _, result, _=mentors_crud.read(
+    _, result, error = mentors_crud.read(
         [
             ["mentor_id", "==", mentor_id]
         ]
     )
-    return result[0] if result else None
+    print(error, flush=True)
+    return MentorResponse.model_validate(result[0])
+
 
 @router.put("/admin/{mentor_id}", response_model=MentorResponse)
 async def update_mentor(mentor_id: str, request: UpdateMentorRequest):
     """Mentor情報を更新"""
-    _, result, _=mentors_crud.update(
+    _, result, error = mentors_crud.update(
         [
             ["mentor_id", "==", mentor_id]
         ],
         request.model_dump(exclude_unset=True)
     )
-    return result
+    print(error, flush=True)
+    return MentorResponse.model_validate(result[0])
 
 
 @router.delete("/admin/{mentor_id}", response_model=MentorResponse)
 async def delete_mentor(mentor_id: str):
     """Mentor情報を削除"""
-    _, result, _=mentors_crud.delete(
+    _, result, error = mentors_crud.delete(
         [
             ["mentor_id", "==", mentor_id]
         ]
     )
-    return result
+    print(error, flush=True)
+    return MentorResponse.model_validate(result[0])
 
 # ====================
 # Mentor Groups
@@ -60,41 +71,74 @@ async def delete_mentor(mentor_id: str):
 @router.post("/groups/new", response_model=NewMentorGroupResponse)
 async def create_group(request: NewMentorGroupRequest):
     """新しいMentorグループを作成"""
-    _, result, _=mentor_groups_crud.create(request)
-    return result
+    group_data = request.model_dump(exclude={"mentors"})
+    group_instance = MentorGroupTableSchema(**group_data)
+    _, group_result, error = mentor_groups_crud.create(group_instance)
+    print(error, flush=True)
+    group_id = group_result.group_id
+
+    """メンバー登録"""
+    added_mentors = []
+    for mentor_info in request.mentors:
+        member_data = {
+            "group_id": group_id,
+            "mentor_id": mentor_info.mentor_id,
+            "role": mentor_info.mentor_id
+        }
+        _, member_result, error = mentor_group_members_crud.create(member_data)
+        print(error, flush=True)
+        added_mentors.append(member_result)
+    return NewMentorGroupResponse.model_validate(group_result)
+
 
 @router.get("/groups/{group_id}", response_model=MentorGroupResponse)
 async def get_group(group_id: str):
     """Mentorグループの情報を取得"""
-    _, result, _=mentor_groups_crud.read(
+    _, group_result, error = mentor_groups_crud.read(
         [
             ["group_id", "==", group_id]
         ]
     )
-    return result[0] if result else None
+    print(error, flush=True)
+    group = group_result[0]
+
+    _, members, error = mentor_group_members_crud.read(
+        [
+            ["group_id", "==", group_id]
+        ]
+    )
+    print(error, flush=True)
+    mentors = [MentorInGroup(name=member.name, mentor_id=member.mentor_id) for member in members]
+    return MentorGroupResponse(
+        name=group.name,
+        description=group.description,
+        mentors=mentors
+    )
 
 
 @router.put("/groups/{group_id}", response_model=MentorGroupResponse)
 async def update_group(group_id: str, request: UpdateMentorGroupRequest):
     """Mentorグループの情報を更新"""
-    _, result, _=mentor_groups_crud.update(
+    _, result, error = mentor_groups_crud.update(
         [
             ["group_id", "==", group_id]
         ],
         request.model_dump(exclude_unset=True)
     )
-    return result
+    print(error, flush=True)
+    return MentorGroupResponse.model_validate(result[0])
+
 
 @router.delete("/groups/{group_id}", response_model=MentorGroupResponse)
 async def delete_group(group_id: str):
     """Mentorグループを削除"""
-    _, result, _=mentor_groups_crud.delete(
+    _, result, error = mentor_groups_crud.delete(
         [
             ["group_id", "==", group_id]
         ]
     )
-    return result
-
+    print(error, flush=True)
+    return MentorGroupResponse.model_validate(result[0])
 
 # ====================
 # Mentor Group Members
@@ -103,20 +147,31 @@ async def delete_group(group_id: str):
 @router.post("/groups/{group_id}/add_mentor", response_model=MentorToGroupResponse)
 async def add_mentor_to_group(group_id: str, request: MentorToGroupRequest):
     """Mentorをグループに追加"""
-    added_mentors=[]
-    for mentor_id in request.mentors:
-        data={"group_id": group_id, "mentor_id": mentor_id}
-        _, result, _=mentor_group_members_crud.create(data)
-        added_mentors.append(result)
-    return {"mentors": added_mentors}
+    added_mentors = []
+    for mentor_info in request.mentors:
+        data = {
+            "group_id": group_id,
+            "mentor_id": mentor_info.mentor_id,
+            "role": mentor_info.role
+            }
+        _, result, error = mentor_group_members_crud.create(data)
+        print(error, flush=True)
+        added_mentors.append(MentorRoleInfo(mentor_id=result.mentor_id, role=result.role))
+    return MentorToGroupResponse(mentors=added_mentors)
+
 
 @router.post("/groups/{group_id}/remove_mentor", response_model=MentorToGroupResponse)
 async def remove_mentor_from_group(group_id: str, request: MentorToGroupRequest):
     """Mentorをグループから削除"""
-    _, result, _=mentor_group_members_crud.delete(
-        [
-            ["group_id", "==", group_id],
-            ["mentor_id", "in", request.mentors]
-        ]
-    )
-    return {"mentors": result}
+    removed_mentors=[]
+    for mentor_id in request.mentors:
+        _, result, error = mentor_group_members_crud.delete(
+            [
+                ["group_id", "==", group_id],
+                ["mentor_id", "==", mentor_id]
+            ]
+        )
+    print(error, flush=True)
+    if result:
+        removed_mentors.append(MentorRoleInfo(mentor_id=mentor_id, role="member"))
+    return MentorToGroupResponse(mentors=result)
